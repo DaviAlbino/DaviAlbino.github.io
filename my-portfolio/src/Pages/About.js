d0eb553d4bbfb0fdace64db690d8c62be674473a
Davi Albino <negroalbino_@hotmail.com>
2023-01-05 19:09:16 -0300

    Deploying

--- my-portfolio/src/Pages/About.js
+++ my-portfolio/src/Pages/About.js
@@ -7,18 +7,20 @@ function About() {
   return (
     <div className="About">
       <Header />
-      <div className="about-container">
-        <img src={require('../images/DaviOffice.jpeg')} alt="My Office" className='my-office' /> 
-        <div className="hello-presentation">
-            <div className="presentation">
-                <text className="welcome">
-                  <p className="part-one">After living experiences in the manufacturing industry, I've decided to accept the great challenge of changing my career path and joining the Web Development area.<br /></p>
-                  <p className="part-one">Once I got in touch with this area, I fell in love with coding, which is constantly stimulating my creativity and logical thinking, skills that I've always liked to work on and to improve.<br /></p>
-                  <p className="part-one">And it is in the programming area where I seek to collaborate with a company that has clear and defined values, where I can contribute to its growth, and thus also contribute to mine.<br /></p>
-                  <p className="part-one">I am currently studying for Full-stack Web Development at Trybe. <br /></p>
-                  <p className="part-one">The technologies I am currently learning and developing are JavaScript, React, CSS3, HTML5, Git, and Docker. <br /></p>
-                </text>
-            </div>
+      <div className="my-about">
+        <div className="about-container">
+          <img src={require('../images/DaviOffice.jpeg')} alt="My Office" className='my-office' /> 
+          <div className="hello-presentation">
+              <div className="presentation">
+                  <text className="welcome">
+                    <p className="part-one">After living experiences in the manufacturing industry, I've decided to accept the great challenge of changing my career path and joining the Web Development area.<br /></p>
+                    <p className="part-one">Once I got in touch with this area, I fell in love with coding, which is constantly stimulating my creativity and logical thinking, skills that I've always liked to work on and to improve.<br /></p>
+                    <p className="part-one">And it is in the programming area where I seek to collaborate with a company that has clear and defined values, where I can contribute to its growth, and thus also contribute to mine.<br /></p>
+                    <p className="part-one">I am currently studying for Full-stack Web Development at Trybe. <br /></p>
+                    <p className="part-one">The technologies I am currently learning and developing are JavaScript, React, CSS3, HTML5, Git, and Docker. <br /></p>
+                  </text>
+              </div>
+          </div>
         </div>
       </div>
       <Footer />
